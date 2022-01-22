574bcd57d8db22de9d98a9d23bcb6036105f2686
Prajjwal Sahu <prajjwalsahu5432@gmail.com>
2022-01-22 16:42:25 +0530

    Clustering in Action

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const cluster = require("cluster");
 
-const express = require("express");
-const app = express();
-
 // If the file is being executed in Master mode
-if (cluster.isMaster) {
+if (cluster.isPrimary) {
    //Cause index.js to be excecuted again in child mode
    cluster.fork();
+   cluster.fork();
+   cluster.fork();
+   cluster.fork();
 } else {
+   const express = require("express");
+   const app = express();
+
    //Im a child, Im going to act like a server and do nothing else
    function dowork(duration) {
       let start = Date.now();
@@ -18,6 +21,10 @@ if (cluster.isMaster) {
    app.get("/", (req, res) => {
       dowork(5000);
       res.send("Hi there");
+   });
+
+   app.get("/fast", (req, res) => {
+      res.send("This was fast");
    });
 
    app.listen(3000);
