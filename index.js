d67b3fb7c51511501f92c0558bff5b148d2d5a0a
Prajjwal Sahu <prajjwalsahu5432@gmail.com>
2022-01-22 16:00:44 +0530

    Forking Children

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const express = require("express");
+const cluster = require("cluster");
 
+const express = require("express");
 const app = express();
 
-function dowork(duration) {
-   let start = Date.now();
+// If the file is being executed in Master mode
+if (cluster.isMaster) {
+   //Cause index.js to be excecuted again in child mode
+   cluster.fork();
+} else {
+   //Im a child, Im going to act like a server and do nothing else
+   function dowork(duration) {
+      let start = Date.now();
 
-   while (Date.now() - start < duration) {}
+      while (Date.now() - start < duration) {}
+   }
+
+   app.get("/", (req, res) => {
+      dowork(5000);
+      res.send("Hi there");
+   });
+
+   app.listen(3000);
 }
-
-app.get("/", (req, res) => {
-   dowork(5000);
-   res.send("Hi there");
-});
-
-app.listen(3000);
